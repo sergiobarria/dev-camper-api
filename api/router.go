c8c75e92d4bff575336164f91920c2e526769d53
sergiobarria <sbarria.dev@gmail.com>
2023-03-30 01:24:59 -0500

    chore: add cors

--- api/router.go
+++ api/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/sergiobarria/dev-camper-api/api/handlers"
 	"github.com/sergiobarria/dev-camper-api/utils"
 	"github.com/spf13/viper"
@@ -17,12 +18,23 @@ func RegisterRoutes() *chi.Mux {
 	// ====== REGISTER MIDDLEWARES ======
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-
 	if debug {
 		r.Use(middleware.Logger)
 	}
 	r.Use(middleware.Recoverer) // Recover must go after Logger
 
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	// ====== REGISTER ROUTES ======
 	r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		utils.NewJSONResponse(w, map[string]interface{}{
 			// "success": true,
@@ -31,7 +43,7 @@ func RegisterRoutes() *chi.Mux {
 		})
 	})
 
-	// ====== Bootcamps ======
+	// ====== Bootcamps Routes ======
 	r.Get("/bootcamps", handlers.GetBootcamps)
 	r.Post("/bootcamps", handlers.CreateBootcamp)
 	r.Get("/bootcamps/{id}", handlers.GetBootcamp)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
 	github.com/spf13/viper v1.15.0
 )
 
