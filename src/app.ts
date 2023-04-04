bc1543d949918edaf1df396103430e47e504ded9
sergiobarria <sbarria.dev@gmail.com>
2023-04-04 10:59:53 -0500

    commit missing file

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
-import express from 'express'
+import express, { type Request, type Response, type NextFunction } from 'express'
+import config from 'config'
 
 import { router } from './router'
-import { morganMiddleware } from './middleware'
+import { globalErrorHandler, morganMiddleware } from './middleware'
+import { APIError } from './lib'
 
 export const app = express()
+const env = config.get('NODE_ENV')
 
 // ===== Apply middlewares 👇🏼 =====
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'development') {
+if (env === 'development') {
     app.use(morganMiddleware)
 }
 
 // ===== Apply routes 👇🏼 =====
 app.use('/api/v1', router)
 
+app.all('*', (req: Request, _: Response, next: NextFunction) => {
+    next(APIError.notFound(`Can't find ${req.originalUrl} on this server!`))
+})
+
 // ===== Apply error handlers 👇🏼 =====
+app.use(globalErrorHandler)
