161b2eae15e4669e700321cedcc3f6171d83cdd0
sergiobarria <sbarria.dev@gmail.com>
2023-04-05 22:05:06 -0500

    add missing file

---
+++ src/router/bootcamps/bootcamps.services.ts
@@ -0,0 +1,40 @@
+import { prisma } from '@/lib'
+import type { Prisma, bootcamps } from '@prisma/client'
+import type { CreateBootcampType } from './bootcamps.schemas'
+
+export const createOne = async (data: CreateBootcampType): Promise<bootcamps> => {
+    const record = await prisma.bootcamps.create({ data })
+
+    return record
+}
+
+export const findFirst = async (filter: Prisma.bootcampsWhereInput): Promise<bootcamps | null> => {
+    const record = await prisma.bootcamps.findFirst({ where: filter })
+
+    return record
+}
+
+export const findMany = async (): Promise<bootcamps[]> => {
+    const records = await prisma.bootcamps.findMany()
+
+    return records
+}
+
+export const findOneById = async (id: string): Promise<bootcamps | null> => {
+    const record = prisma.bootcamps.findUnique({ where: { id } })
+
+    return record
+}
+
+export const updateOne = async (
+    id: string,
+    data: Partial<CreateBootcampType>
+): Promise<bootcamps | null> => {
+    const record = await prisma.bootcamps.update({ where: { id }, data })
+
+    return record
+}
+
+export const deleteOne = async (id: string): Promise<void> => {
+    await prisma.bootcamps.delete({ where: { id } })
+}
